3f0bff6838e71e2098222bafeba3aed6ba81bfc0
GitHub-yfqs <1360661056@qq.com>
2018-12-13 10:05:25 +0800

    1

--- src/test/LearnHashMap.java
+++ src/test/LearnHashMap.java
@@ -34,22 +34,31 @@ public class LearnHashMap {
 		 */
 		/*System.out.println(LearnHashMap.tableSizeFor(8));*/
 		
+		
 		/*
 		 * 2018/12/11
 		 * 其抛出ConcurrentModificationException
 		 * 把"GoddessY".equals(a)换成"Joemsu".equals(a)还会抛出异常吗？
 		 * https://www.cnblogs.com/joemsu/p/7724623.html#_caption_2
+		 * 
+		 * 而不推荐也不确保在生产环境中可以得到正确结果的这个异常：
+			ConcurrentModificationException：
+			（1） 如果一个线程正在迭代遍历一个集合Collection.另一个线程同时修改了此Collection内的值，引起了“结构性”变化，那么这个异常就会抛出.
+			对应的迭代器（iterator）是一种快速失败的迭代器（fail-fast，相对于冒着风险继续遍历的迭代器来讲）.
+			（2）上面是多线程并发的情形.这个异常并不是只在上述情况下抛出.即使单线程的场景，这个异常也是有可能抛出的.
+			比如：一个线程正在利用迭代器遍历集合，但同时修改了集合的值，引起了集合的结构性变化，此异常亦会抛出.
+			所以那个modCount是用于探测某集合被迭代遍历时，其结构或者内部布局是否发生了变化.
 		 */
 		Map<String, Integer> map = new HashMap<>();
 		map.put("GoddessY", 1);
 		map.put("Joemsu", 2);
 		for (String a : map.keySet()) {
-		  /*if ("GoddessY".equals(a)) {
+		  if ("GoddessY".equals(a)) {
 		    map.remove(a);
-		  }*/
-			if ("Joemsu".equals(a)) {
+		  }
+			/*if ("Joemsu".equals(a)) {
 			    map.remove(a);
-			  }
+			  }*/
 		}
 		
 		
